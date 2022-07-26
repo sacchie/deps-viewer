34dadd80bfe4500c5d9cee16f07e16f3f0957a74
Kaiichiro Ota <kaiichiro-ota@cybozu.co.jp>
2022-07-26 18:38:06 +0900

    fill window by color

--- window.py
+++ window.py
@@ -1,9 +1,23 @@
 import gi
+import random
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 win = Gtk.Window()
 win.connect("destroy", Gtk.main_quit)
+
+rand = random.Random()
+
+def on_draw(area, context):
+    context.scale(area.get_allocated_width(), area.get_allocated_height())    
+    context.set_source_rgb(rand.random(), 0.5, 0.7)
+    context.fill() 
+    context.paint()
+
+area = Gtk.DrawingArea()
+win.add(area)
+area.connect("draw", on_draw)
+
 win.show_all()
 Gtk.main()
